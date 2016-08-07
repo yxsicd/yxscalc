a0a4c81dff1ef6e4d6420a36766aafc0118018ac
yxsicd <yxsicd@163.com>
2016-08-07 09:20:34 +0800

    撒旦法

---
+++ dev/kuaidi.js
@@ -0,0 +1,112 @@
+
+var alltranstring='{"":"","EMS快递":"ems","申通快递":"shentong","顺丰快递":"shunfeng","圆通快递":"yuantong","韵达快递":"yunda","百世汇通快递":"huitong","天天快递":"tiantian","中通快递":"zhongtong","宅急送快递":"zhaijisong","中国邮政":"pingyou","全峰快递":"quanfeng","国通快递":"guotong","京东快递":"jingdong","速尔快递":"sure","快捷快递":"kuaijie","日日顺物流":"ririshun","中铁快运":"zhongtie","优速快递":"yousu","龙邦快递":"longbang","德邦物流":"debang","如风达快递":"rufeng","联昊通快递":"lianhaotong","国际Fedex":"fedex","Fedex国内":"fedexcn","DHL快递":"dhl","信丰快递":"xinfeng","E邮宝":"eyoubao","忠信达快递":"zhongxinda","长通物流":"changtong","USPS快递":"usps","华企快递":"huaqi","芝麻开门快递":"zhima","邮政小包":"gnxb","尼尔快递":"nell","增益快递":"zengyi","宇鑫物流":"yuxin","星程宅配快递":"xingchengzhaipei","安能物流":"anneng","大达物流":"dada","高考录取通知书":"tongzhishu","AOL快递":"aol","成都东骏快递":"dongjun","苏宁快递":"suning","UPU-Universal Postal Union":"upu","全一快递":"quanyi","华宇物流":"huayu","全日通快递":"quanritong","丰程物流":"fengcheng","民航快递":"minhang","中邮物流":"zhongyou","万家物流":"wanjia","佳吉快运":"jiaji","万象物流":"wanxiang","贝海国际快递":"beihai","墨尔本骏丰快递":"junfeng","骏达快递":"junda","全信通快递":"quanxintong","UPS快递":"ups","TNT快递":"tnt","一邦快递":"yibang","盛辉物流":"shenghui","亚风快递":"yafeng","D速快递":"dsu","大田物流":"datian","佳怡物流":"jiayi","加运美快递":"jiayunmei","全晨快递":"quanchen","OCS快递":"ocs","盛丰物流":"shengfeng","新邦物流":"xinbang","程光快递":"chengguang","丰达快递":"fengda","原飞航物流":"feihang","晋越快递":"jinyue","越丰快递":"yuefeng","安捷快递":"anjie","AAE快递":"aae","运通中港快递":"yuntong","DPEX快递":"dpex","远成物流":"yuancheng","广东邮政物流":"gdyz","Aramex国际快递":"aramex","国际邮政快递":"intmail","北京一统飞鸿快递":"ytfh","乐捷递快递":"lejiedi","三态速递":"santai","传志快递":"chuanzhi","共速达物流|快递":"gongsuda","百福东方物流":"ees","伟邦(SCS)快递":"scs","平安达":"pinganda","源安达快递":"yad","递四方速递":"disifang","顺捷丰达快递":"yinjie","嘉里大通物流":"jldt","东方快递":"coe","传喜快递":"chuanxi","飞豹快递":"feibao","京广快递":"jingguang","飞远物流":"feiyuan","城市之星":"cszx","RPX保时达":"rpx","CityLinkExpress":"citylink","城市100":"chengshi100","成都立即送快递":"lijisong","巴伦支":"balunzhi","大洋物流快递":"dayang","店通快递":"diantong","凡宇快递":"fanyu","昊盛物流":"haosheng","河北建华快递":"hebeijianhua","急先达物流":"jixianda","穗佳物流":"suijia","圣安物流":"shengan","赛澳递":"saiaodi","山东海红快递":"haihong","微特派":"weitepai","城际快递":"chengji","Fardar":"fardar","恒路物流":"henglu","海外环球快递":"hwhq","金大物流":"jinda","跨越快递":"kuayue","顺鑫(KCS)快递":"kcs","明亮物流":"mingliang","民邦快递":"minbang","闽盛快递":"minsheng","希优特快递":"xiyoute","祥龙运通快递":"xianglong","亿顺航快递":"yishunhang","成都奔腾国际快递":"benteng","济南中天万运":"zhongtian","郑州建华快递":"zhengzhoujianhua","飞特物流":"feite","华翰物流":"huahan","宝通达":"baotongda","出口易物流":"chukouyi","誉美捷快递":"yumeijie","宽容物流":"kuanrong","南北快递":"nanbei","万博快递":"wanbo","速呈宅配":"suchengzhaipei","晟邦物流":"shengbang","百千诚国际物流":"baiqian","高铁快递":"gaotie","冠达快递":"guanda","好来运快递":"haolaiyun","户通物流":"hutong","华航快递":"huahang","黄马甲快递":"huangmajia","合众速递":"ucs","捷特快递":"jiete","久易快递":"jiuyi","快优达速递":"kuaiyouda","蓝弧快递":"lanhu","门对门快递":"menduimen","陪行物流":"peixing","日昱物流":"riyu","上海林道货运":"lindao","世运快递":"shiyun","奥硕物流":"aoshuo","新顺丰（NSF）快递":"nsf","大金物流":"dajin","中国远洋运输(COSCON)":"coscon","宇宏物流":"yuhong","佳宇物流":"jiayu","港快速递":"gangkuai","快淘速递":"kuaitao","速通物流":"sutong","安迅物流":"anxun","香港邮政":"hkpost","日本邮政":"jppost","新加坡邮政(Sing Post)":"singpost","中铁物流":"ztwl","贝邮宝":"ppbyb","燕文物流":"yanwen","飞洋快递":"feiyang","佐川急便":"zuochuan","恒宇运通":"hengyu","蒙速快递":"mengsu","五环速递":"wuhuan","思迈快递":"simai","佳惠尔快递":"jiahuier","安得物流":"ande","荣庆物流":"rongqing","大顺物流":"dashun","方方达物流":"fangfangda","汇文快递":"huiwen","速捷快递":"sujie","德国DHL快递":"dhlde","百腾物流":"baiteng","DCS快递":"dcs","DPD快递":"dpd","腾迅达物流":"tengxunda","品骏快递":"pinjun","蓝天快递":"bse","UEQ快递":"ueq","XRU快递":"xru","远成快运":"ycky","爱尔兰邮政(AN POST)":"anpost","韩国邮政":"koreapost","挪威邮政":"posten","澳大利亚邮政":"auspost","意大利邮政":"posteit","新西兰邮政(NZPost)":"nzpost","比利时邮政(BPost)":"bpost","德国邮政(Deutsche Post)":"depost","西班牙邮政":"correos","荷兰邮政":"postnl","泰国邮政":"thailand","北京邮政快递":"bjems","加拿大邮政":"canpost","丹麦邮政":"postdanmark","俄速通快递":"ruston","俄罗斯邮政":"pochta","乌克兰邮政":"ukrposhta","马来西亚邮政":"posmy","芬兰邮政":"postifi","印度邮政":"indiapost","波兰邮政":"pocztex","泛捷国际快递":"fanjie","八达通快递":"8dt","易客满快递":"yikeman","速派快递":"fastgo","中邮快递":"cnpex","澳邮中国快运":"auexpress","PCA Express":"pca","韩国ADP快递":"adp","A2U快递":"a2u","笨鸟快递":"birdex","爱沙尼亚邮政":"omniva","突尼斯邮政":"laposte","瑞士邮政":"swisspost","明大快递":"ada","美国申通快递":"stous","Quantium":"quantium","OnTrac快递":"ontrac","希腊邮政":"eltacourier","全通快递":"quantong","TNT UK":"tntuk","捷记方舟快递":"ajexpress","佳成国际快递":"jcex","葡萄牙邮政":"ctt","GLS快递":"gls","GATI快递":"gaticn","南非邮政":"southafricanpost","Gati-KWE快递":"gatikwe","GSM快递":"gsm","UPS i-parcel":"iparcel","巴西邮政":"brazilcorreios","沙特邮政":"saudipost","柬埔寨邮政_Cambodia Post":"cambodiapost","越南邮政_Vietnam Post":"vnpost","Chronopost Portugal":"chronopost-portugal","捷买送(Buy Logic)快递":"buylogic","台湾邮政(Taiwan Post)":"taiwan-post","Speed Post":"speedpost","老挝邮政":"laopost","阿联酋邮政(Emirates Post)":"emirates-post","Cuckoo布谷鸟快递":"cuckoo","DHL eCommerce":"dhl-global-mail","Asendia USA":"asendia-usa","日本黑猫宅急便(Yamato Japan)":"yamato-japan","China Post":"china-post","英国皇家邮政(Royal Mail)":"royal-mail","Parcel Force":"parcel-force","DPD Ireland":"dpd-ireland","DPD Germany":"dpd-de","捷克邮政(Czech Post - Česká pošta)":"czech-post","Go Fly":"gofly","Chronopost France":"chronopost-france","法国邮政(La Poste)":"laposte-france","智利邮政(Correos Chile)":"correos-chile","锦程国际物流":"jc56","罗马尼亚邮政(Poșta Română)":"posta-romana","白俄罗斯邮政(Belpost)":"belpost","土耳其邮政(PTT Posta)":"ptt-posta","保加利亚邮政(Bulgarian Posts)":"bgpost","冰岛邮政(Iceland Post)":"iceland-post","立陶宛邮政(Lietuvos Paštas)":"lietuvos-pastas","塞浦路斯邮政(Cyprus Post)":"cyprus-post","匈牙利邮政(Magyar Posta)":"magyar-posta","克罗地亚邮政(Hrvatska Pošta)":"hrvatska-posta","瑞典邮政(PostNord)":"postnord","澳门邮政":"macau-post","以色列邮政(Israel Post)":"israel-post","阿根廷邮政(Correo Argentino)":"correo-argentino","尼日利亚邮政(NiPost)":"nigeria-post","巴基斯坦邮政(Pakistan Post)":"pakistan-post","拉脱维亚邮政(Latvijas Pasts)":"latvijas-pasts","卡塔尔邮政(Q Post)":"q-post","印度尼西亚邮政(Pos Indonesia)":"pos-indonesia-int","阿富汗邮政(Afghan Post)":"afghan-post","Nova Poshta":"nova-poshta","黎巴嫩邮政(Liban Post)":"liban-post","塞尔维亚邮政(POŠTA SRBIJE)":"post-serbia","伊朗邮政(Iran Post)":"iran-post","斯洛伐克邮政(Slovenská pošta)":"slovenska-posta","天峰国际物流":"tianfeng","One World 物流":"one-world","云途物流":"yunexpress","孟加拉国邮政(Bangladesh Post)":"bangladesh-post","UK MAIL":"uk-mail","Sky Net":"skynet","法国Colissimo":"colissimo","菲律宾邮政（Philippine Post）":"phlpost","埃及邮政(Egypt Post)":"egypt-post","新西兰Fast Way 快递":"fastway-nz","澳大利亚Fast Way 快递":"fastway-au","新西兰CourierPost快递":"courierpost","爱尔兰Fastway快递":"fastway-ie","奥地利邮政(Austrian Post)":"austrian-post","速丰达快递":"sufengda","易达通快递":"qexpress","CFE 超峰快递":"cfe","翼速物流":"yisu","速翼快递":"suyi","苏驰物流":"suchi","顺达快递":"shunda","方舟快递":"arkexpress","长江国际速递":"changjiang","FOX快递":"fox","美速达快递":"apeexpress","兆速达物流":"zhaosuda","优寄快递":"ushipshop","亿翔快递":"yixiang","飞鹰物流":"feiying","卢森堡邮政(Post Luxembourg)":"post-luxembourg","马耳他邮政(Malta Post)":"maltapost","乌拉圭邮政(Correo Uruguayo)":"correo-uruguayo","Pos Laju":"poslaju","坦桑尼亚邮政(TANZANIA POSTA)":"tanzania-post","百慕大邮政(Bermuda post)":"bermuda-post","秘鲁邮政(Peru Post)":"peru-post","委内瑞拉邮政":"venezuela-post","哥伦比亚邮政":"colombia-post","墨西哥邮政":"correos-mexico","阿尔巴尼亚邮政":"albania-post","荷属安的列斯邮政":"antilles-post","亚美尼亚邮政":"armenia-post","厄瓜多尔邮政":"ecuador-post","卢旺达邮政":"rwanda-post","阿鲁巴邮政":"aruba-post","萨尔瓦多邮政":"el-salvador-post","塞内加尔邮政":"senegal-post","马其顿邮政":"macedonia-post","埃塞俄比亚邮政":"ethiopia-post","马达加斯加邮政":"madagascar-post","阿塞拜疆邮政":"azerbaijan-post","马尔代夫邮政":"maldives-post","斯洛文尼亚邮政":"slovenia-post","巴哈马邮政":"bahamas-post","马里邮政":"mali-post","所罗门群岛邮政":"solomon-islands-post","巴巴多斯邮政":"barbados-post","加蓬邮政":"gabon-post","格鲁吉亚邮政":"georgia-post","毛里塔尼亚邮政":"mauritania-post","毛里求斯邮政":"mauritius-post","贝宁邮政":"benin-post","加纳邮政":"ghana-post","摩尔多瓦邮政":"moldova-post","不丹邮政":"bhutan-post","蒙古邮政":"mongolia-post","波黑邮政":"bosnia-herzegovina-post","圭亚那邮政":"guyana-post","黑山邮政":"montenegro-post","博茨瓦纳邮政":"botswana-post","莫桑比克邮政":"mozambique-post","缅甸邮政":"myanmar-post","乌干达邮政":"uganda-post","尼泊尔邮政":"nepal-post","开曼群岛邮政":"cayman-islands-post","乌兹别克斯坦邮政":"uzbekistan-post","科特迪瓦邮政":"ivory-coast-post","巴布亚新几内亚邮政":"papua-new-guinea-post","瓦努阿图邮政":"vanuatu-post","牙买加邮政":"jamaica-post","巴拉圭邮政":"paraguay-post","哥斯达黎加邮政":"costa-rica-post","约旦邮政":"jordan-post","赞比亚邮政":"zambia-post","古巴邮政":"cuba-post","哈萨克斯坦邮政":"kazakhstan-post","津巴布韦邮政":"zimbabwe-post","肯尼亚邮政":"kenya-post","摩洛哥邮政":"morocco-post","文莱邮政":"brunei-post","多米尼加邮政(INPOSDOM)":"dominican-post","新干线快递":"anlexpress","澳洲新干线快递":"expressplus","能达快递":"nengda","瑞丰速递":"ruifeng","速腾快递":"suteng","纵行物流":"zongxing","京世物流":"jingshi","华诚物流":"huacheng"}';
+    var alltran=JSON.parse(alltranstring);
+    
+    function getid(name)
+    {
+        if(alltran[name]){
+            return alltran[name];
+        }
+
+        var kw=["\r","\n","速运","快递","快运","物流","速递","快运"];
+
+        var map={
+            "信丰新版":"信丰","優速快遞":"优速","百事":"百世"
+        }
+
+       for(var key in alltran)
+       {
+           var sname=name.toLowerCase().trim();
+           var sname1=name.toLowerCase().trim();
+
+           var sname2=sname;
+           for(var w in kw)
+           {
+                sname2=sname2.replace(kw[w],"");
+           }
+
+           var sname3=sname;
+            for(var w in map)
+           {
+                sname3=sname3.replace(w,map[w]);
+           }
+
+           var sname4=sname3;
+            for(var w in kw)
+           {
+               sname4=sname4.replace(kw[w],"");
+           }
+
+           var skey = key.toLowerCase();
+           if(skey.match(sname))
+           {
+               return alltran[key];
+           }
+           if(skey.match(sname1))
+           {
+               return alltran[key];
+           }
+           if(skey.match(sname2))
+           {
+               return alltran[key];
+           }
+           if(skey.match(sname3))
+           {
+               return alltran[key];
+           }
+            if(skey.match(sname4))
+           {
+               return alltran[key];
+           }
+       }
+    }
+
+    function geturl(name,no)
+    {
+        var url="http://biz.trace.ickd.cn/";
+        var url2="?callback=addret";
+        var id=getid(name);
+        var ret=url+id+"/"+no.trim()+url2;
+        return ret;
+    }
+
+    function dosafe()
+    {
+        
+    }
+
+
+    function addret(d)
+    {
+        addret.ret?0:addret.ret={};
+        var key=JSON.stringify({expTextName:d.expTextName,mailNo:d.mailNo});
+        addret.ret[key]=d;
+    }
+
+    function donothing()
+    {
+
+    }
+
+    function myquery()
+    {
+        addret.ret={};
+        var srcarr=src_1.getValue().split("\n");
+        for(var a in srcarr)
+        {
+            var line=srcarr[a];
+            var linearr=line.split("\t");
+            var cid=linearr[0];
+            var name=linearr[1];
+            var no=linearr[2];
+            if(!name){continue;}
+            var url=geturl(name,no);
+            $.getScript(url);
+            /*
+            $.getScript("http://i.ickd.cn/userLog.do?add&callback=donothing&com="+getid(name)+"&mailNo="+no+"&status=3&_=1470531237488",function(d){
+                $.getScript(url);
+            })*/
+           
+        }
+
+    }
